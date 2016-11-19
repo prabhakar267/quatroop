a36b3c3fd45c26ff07349d3f6ac654feb1b78500
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-11-20 03:47:58 +0530

    view user interface complete

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -11,21 +11,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller{
     
-    private function _getAllUsers(){
+    private function _getAllUsers($mode=false){
         $repository = $this->getDoctrine()->getRepository("AppBundle\Entity\User");
 
         $users_array = array();
         $users = $repository->findBy(array(), array('user_name' => 'ASC'));
 
         foreach($users as $user){
-            $user_temp = array(
-                'id' => $user->user_id,
-                'name' => $user->user_name,
-            );
-            array_push($users_array, $user_temp);
+            if($mode){
+                $user_temp = array(
+                    'id' => $user->user_id,
+                    'name' => $user->user_name,
+                    'parents' => json_decode($user->parents),
+                    'children' => json_decode($user->children),
+                );
+                array_push($users_array, $user_temp);
+            } else {
+                $user_temp = array(
+                    'id' => $user->user_id,
+                    'name' => $user->user_name,
+                );
+                array_push($users_array, $user_temp);
+            }
         }
 
         return $users_array;
+    }
+
+    private function _getUserObjectsFromIDs($all_users, $users_array){
+        $output_array = [];
+        if(empty($users_array))
+            return $output_array;
+
+        foreach($users_array as $user_id){
+            $random_num = rand(1,7);
+            $temp_user = array(
+                'id' => $user_id,
+                'name' => $all_users[$user_id],
+                'image' => "img/random_users/user" . $random_num . "-128x128.jpg",
+            );
+
+            array_push($output_array, $temp_user);
+        }
+        return $output_array;
     }
 
     /**
@@ -37,7 +65,6 @@ class UserController extends Controller{
         $request = Request::createFromGlobals();
 
         if($request->getMethod() == 'GET'){
-
             return $this->render('admin/add-user.html.twig', array(
                 "all_users" => $this->_getAllUsers(),
             ));
@@ -84,11 +111,80 @@ class UserController extends Controller{
         }
     }
 
+
     /**
      * @Route("/user", name="viewuser")
      */
     public function viewUserAction(){
-        return $this->render('admin/view-user.html.twig', array());
+        $request = Request::createFromGlobals();
+
+        if($request->getMethod() == 'GET'){
+            return $this->render('admin/view-user.html.twig', array(
+                "all_users" => $this->_getAllUsers(),
+            ));
+        } else {
+            $user_id = (int)$request->request->get('user');
+            
+            return $this->redirectToRoute('view_one_user', array(
+                'user_id' => $user_id,
+            ));
+        }
+    }
+
+
+    /**
+     * @Route("/user/{user_id}", name="view_one_user")
+     */
+    public function viewOneUserAction($user_id){
+        // check if the user id is a postitive integer or not
+        if((int)$user_id <= 0){
+            return $this->redirectToRoute('viewuser');
+        }
+        
+        $all_users_information = $this->_getAllUsers($mode=true);
+
+        $immediate_parents = [];
+        $immediate_children = [];
+
+        $parents = [];
+        $children = [];
+        $names = [];
+        $user_existence_flag = false;
+
+        foreach($all_users_information as $temp_user){
+            $id = $temp_user["id"];
+            
+            if($id == $user_id)
+                $user_existence_flag = true;
+
+            $parents[$id] = $temp_user['parents'];
+            $children[$id] = $temp_user['children'];
+            $names[$id] = $temp_user['name'];
+        }
+
+
+        if(!$user_existence_flag)
+            return $this->render('admin/view-user.html.twig', array(
+                "user_success" => true,
+                "info_success" => false,
+                "message" => "Sorry, we could not find a user with ID : <strong>" . $user_id . "</strong>",
+                "all_users" => $all_users_information,
+            ));
+        else {
+            $immediate_children = $this->_getUserObjectsFromIDs($names, $children[$user_id]);
+            $immediate_parents = $this->_getUserObjectsFromIDs($names, $parents[$user_id]);
+
+            // echo json_encode($immediate_parents);
+            // die;
+            return $this->render('admin/view-user-success.html.twig', array(
+                "user_name" => $names[$user_id],
+                "all_parents" => $immediate_parents,
+                "all_children" => $immediate_children,
+                "immediate_parents" => $immediate_parents,
+                "immediate_children" => $immediate_children,
+            ));
+        }
+
     }
 
 }
