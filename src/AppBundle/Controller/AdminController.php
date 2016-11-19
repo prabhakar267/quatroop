54dbd4c592b74f34a8d581f135b16e0e13dd0cbf
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-11-19 23:03:04 +0530

    task templating complete

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -8,10 +8,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
+    
+    /**
+     * @Route("/github-repo-redirect", name="github_repo")
+     */
+    public function githubRedirectAction()
+    {
+        return $this->redirect('https://github.com/prabhakar267/dquip-task');
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function numberAction()
+    public function homepageAction()
     {
         // if(is_numeric($number))
             return $this->render('admin/index.html.twig', array(
